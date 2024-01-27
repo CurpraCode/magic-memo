ada96d5da5d7a78f114c9d67071ea25f17d1dd32
CurpraCode <curtsoyaks@gmail.com>
2024-01-27 21:07:58 +0100

    fix(app auth): resolve the protect route issue

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,6 +1,13 @@
 import CreateMemo from "@/components/creatememo";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/utils/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return redirect("/auth");
+  }
   return (
     <div>
       <CreateMemo />
--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,11 +1,14 @@
 import MemoContainer from "@/components/memocontainer";
 import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/utils/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
   const data = await prisma.memo.findMany();
-  return (
-    <div>
-      <MemoContainer memoViewData={data} />
-    </div>
-  );
+  if (!session) {
+    return redirect("/auth");
+  }
+  return <div>{session ? <MemoContainer memoViewData={data} /> : null}</div>;
 }
